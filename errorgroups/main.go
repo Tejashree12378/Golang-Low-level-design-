2c617693124dc48605e1a05df675d47fc341d372
Tejashree Venkatesh <tejashreevenkatesh@Tejashrees-MacBook-Air.local>
2025-12-30 11:48:10 -0500

    add support for return on ctx done

--- errorgroups/main.go
+++ errorgroups/main.go
@@ -17,18 +17,20 @@ const (
 func main() {
 	jobs := make([]job, 10)
 	for i := 1; i <= 10; i++ {
-		jobs[i-1] = job{i, "https://www.google.com/"}
+		jobs[i-1] = job{i, "https://www.google.com/", 0}
 	}
 
 	//jobs[5].url = "fksnef.com"
 	//jobs[4].url = "https://httpbin.org/delay/5"
+	jobs[4].sleepTime = 1
 
 	batchProcessor(jobs)
 }
 
 type job struct {
-	id  int
-	url string
+	id        int
+	url       string
+	sleepTime time.Duration
 }
 
 func batchProcessor(jobs []job) error {
@@ -71,31 +73,40 @@ func batchProcessor(jobs []job) error {
 
 func worker(ctx context.Context, jobs <-chan job) error {
 	for j := range jobs {
-		if ctx.Err() != nil {
+		errCh := make(chan error, 1)
+
+		go func(j job) {
+			errCh <- executor(ctx, j)
+		}(j)
+
+		select {
+		case err := <-errCh:
+			if err != nil {
+				return err
+			}
+		case <-ctx.Done():
 			return ctx.Err()
 		}
-
-		if j.id%9 == 0 {
-			time.Sleep(1 * time.Second)
-		}
-
-		req, err := http.NewRequestWithContext(ctx, "GET", j.url, nil)
-		if err != nil {
-			return err
-		}
-
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			return err
-		}
-
-		//if j.id%9 == 0 {
-		//	time.Sleep(2 * time.Second)
-		//}
-
-		defer resp.Body.Close()
-		fmt.Println(j.id, resp.StatusCode)
 	}
+
+	return nil
+}
+
+func executor(ctx context.Context, j job) error {
+	time.Sleep(j.sleepTime * time.Second)
+
+	req, err := http.NewRequestWithContext(ctx, "GET", j.url, nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+	fmt.Println(j.id, resp.StatusCode)
 
 	return nil
 }
