fc6bde1ef05103026007e53760f968e455c4d564
Tejashree Venkatesh <tejashreevenkatesh@Tejashrees-MacBook-Air.local>
2026-01-01 19:19:11 -0500

    use conditional variables

--- thread-safe-in-memory-message-queue/main.go
+++ thread-safe-in-memory-message-queue/main.go
@@ -2,96 +2,170 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"math/rand"
 	"sync"
 	"time"
-
-	"golang.org/x/sync/errgroup"
 )
 
+type Message struct {
+	id      int
+	payload string
+}
+
+type Queue interface {
+	Enqueue(msg Message) error
+	Dequeue() (Message, error)
+	Close()
+}
+
+type InMemoryQueue struct {
+	buf      []Message
+	head     int
+	tail     int
+	size     int
+	capacity int
+	closed   bool
+	mu       *sync.Mutex
+	notFull  *sync.Cond
+	notEmpty *sync.Cond
+}
+
+func NewInMemoryQueue(capacity int) Queue {
+	q := &InMemoryQueue{
+		buf:      make([]Message, capacity),
+		head:     0,
+		tail:     0,
+		size:     0,
+		capacity: capacity,
+		closed:   false,
+		mu:       &sync.Mutex{},
+	}
+
+	q.notFull = sync.NewCond(q.mu)
+	q.notEmpty = sync.NewCond(q.mu)
+	return q
+}
+
+func (q *InMemoryQueue) Enqueue(msg Message) error {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	for q.size == q.capacity && !q.closed {
+		q.notFull.Wait()
+	}
+
+	if q.closed {
+		return errors.New("queue is closed")
+	}
+
+	q.buf[q.tail] = msg
+	q.tail = (q.tail + 1) % q.capacity
+	q.size += 1
+
+	q.notEmpty.Signal()
+
+	return nil
+}
+
+func (q *InMemoryQueue) Dequeue() (Message, error) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	for q.size == 0 && !q.closed {
+		q.notEmpty.Wait()
+	}
+
+	if q.size == 0 && q.closed {
+		return Message{}, errors.New("queue is closed")
+	}
+
+	msg := q.buf[q.head]
+	q.head = (q.head + 1) % q.capacity
+	q.size -= 1
+
+	q.notFull.Signal()
+
+	return msg, nil
+}
+
+func (q *InMemoryQueue) Close() {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	if q.closed {
+		return
+	}
+
+	q.closed = true
+
+	q.notFull.Broadcast()
+	q.notEmpty.Broadcast()
+}
+
+func producer(ctx context.Context, q Queue, wg *sync.WaitGroup) {
+	defer wg.Done()
+	id := 0
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			msg := Message{id: id, payload: "random"}
+			if err := q.Enqueue(msg); err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			id++
+			time.Sleep(time.Duration(rand.Intn(300)) * time.Millisecond)
+		}
+	}
+}
+
+func consumer(ctx context.Context, q Queue, wg *sync.WaitGroup) {
+	defer wg.Done()
+	id := 0
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			msg, err := q.Dequeue()
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			fmt.Println("message received ", msg.id, msg.payload)
+			id++
+			time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+		}
+	}
+}
+
 const (
-	NUM_WORKERS = 5
+	NUM_WORKERS = 1
 	TIMEOUT     = 20 * time.Second
+	CAPACITY    = 10
 )
 
 func main() {
-	dispatcher()
-}
+	q := NewInMemoryQueue(CAPACITY)
+	wg := &sync.WaitGroup{}
 
-func dispatcher() {
-	signal := make(chan int)
-	parentCtx, cancel := context.WithTimeout(context.Background(), TIMEOUT)
+	wg.Add(NUM_WORKERS * 2)
+
+	ctx, cancel := context.WithTimeout(context.Background(), TIMEOUT)
 	defer cancel()
 
-	var eg, ctx = errgroup.WithContext(parentCtx)
-
 	for i := 0; i < NUM_WORKERS; i++ {
-		eg.Go(func() error {
-			return consumer(signal, ctx)
-		})
-
-		eg.Go(func() error {
-			return producer(signal, ctx)
-		})
+		go consumer(ctx, q, wg)
+		go producer(ctx, q, wg)
 	}
 
-	if err := eg.Wait(); err != nil {
-		if err.Error() == "context deadline exceeded" {
-			fmt.Printf("processing stopped after %v seconds", TIMEOUT)
-			return
-		}
-
-		fmt.Println(err)
-	}
-}
-
-var (
-	Queue []Event
-	mu    sync.Mutex
-	ID    int
-)
-
-type Event struct {
-	id      int
-	message string
-}
-
-func consumer(signal chan int, ctx context.Context) error {
-	for {
-		select {
-		case <-signal:
-			var msg Event
-
-			mu.Lock()
-			if len(Queue) > 0 {
-				msg = Queue[0]
-				Queue = Queue[1:]
-			}
-
-			fmt.Println(msg.id, msg.message)
-			mu.Unlock()
-
-		case <-ctx.Done():
-			return ctx.Err()
-		}
-	}
-}
-
-func producer(signal chan int, ctx context.Context) error {
-	for {
-		interval := time.Duration(rand.Int31n(10)) * time.Second
-
-		select {
-		case <-time.After(interval):
-			mu.Lock()
-			Queue = append(Queue, Event{id: ID, message: "Order_Created"})
-			ID += 1
-			mu.Unlock()
-
-			signal <- 1
-		case <-ctx.Done():
-			return ctx.Err()
-		}
-	}
+	<-ctx.Done()
+	q.Close()
+	wg.Wait()
 }
